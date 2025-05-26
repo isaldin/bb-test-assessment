c1e6f38648f3d72223547fec634c9305841efd75
ilya saldin <saldin.iliya@gmail.com>
2025-05-26 22:34:26 +0500

    ui: Button & Icon components

---
+++ src/components/ButtonWithIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="button-with-icon">
+    <div class="button-with-icon__icon">
+      <slot name="icon" />
+    </div>
+    <span class="button-with-icon__text">
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.button-with-icon {
+  align-items: center;
+  display: flex;
+  font-size: 13px;
+  font-weight: 600;
+  padding: 4px 8px 4px;
+  border: none;
+  outline: 2px solid rgba(0, 0, 0, 0.05);
+  outline-offset: -2px;
+  border-radius: 12px;
+  height: 24px;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+
+  &__icon {
+    align-items: center;
+    display: flex;
+  }
+
+  &__text {
+    margin-left: 4px;
+  }
+}
+</style>
---
+++ src/components/icons/IconMinus.vue
@@ -0,0 +1,7 @@
+<template>
+  <Icon />
+</template>
+
+<script lang="ts" setup>
+import Icon from '@/assets/svg/icons/minus.svg'
+</script>
